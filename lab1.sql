268522cb6434a5768bbe32dae932c9e2e2a47672
yash patoliya <yashpatoliya14@gmail.com>
2024-12-07 19:41:17 +0530

    change in lab 2

---
+++ lab1.sql
@@ -0,0 +1,228 @@
+﻿create database CSE_4A_113
+--tables schema and insert query
+-- Create Tables
+CREATE TABLE Artists (
+ Artist_id INT PRIMARY KEY,
+ Artist_name NVARCHAR(50)
+);
+
+CREATE TABLE Albums (
+ Album_id INT PRIMARY KEY,
+ Album_title NVARCHAR(50),
+ Artist_id INT,
+ Release_year INT,
+ FOREIGN KEY (Artist_id) REFERENCES Artists(Artist_id)
+);
+
+CREATE TABLE Songs (
+ Song_id INT PRIMARY KEY,
+ Song_title NVARCHAR(50),
+ Duration DECIMAL(4, 2),
+ Genre NVARCHAR(50),
+ Album_id INT,
+ FOREIGN KEY (Album_id) REFERENCES Albums(Album_id)
+);
+
+-- Insert Data into Artists Table
+INSERT INTO Artists (Artist_id, Artist_name) VALUES
+(1, 'Aparshakti Khurana'),
+(2, 'Ed Sheeran'),
+(3, 'Shreya Ghoshal'),
+(4, 'Arijit Singh'),
+(5, 'Tanishk Bagchi');
+
+-- Insert Data into Albums Table
+INSERT INTO Albums (Album_id, Album_title, Artist_id, Release_year) VALUES (1007, 'Album7', 1, 2015),
+(1001, 'Album1', 1, 2019),
+(1002, 'Album2', 2, 2015),
+(1003, 'Album3', 3, 2018),
+(1004, 'Album4', 4, 2020),
+(1005, 'Album5', 2, 2020),
+(1006, 'Album6', 1, 2009);
+
+-- Insert Data into Songs Table
+INSERT INTO Songs (Song_id, Song_title, Duration, Genre, Album_id) VALUES
+(101, 'Zaroor', 2.55, 'Feel good', 1001),
+(102, 'Espresso', 4.10, 'Rhythmic', 1002),
+(103, 'Shayad', 3.20, 'Sad', 1003),
+(104, 'Roar', 4.05, 'Pop', 1002),
+(105, 'Everybody Talks', 3.35, 'Rhythmic', 1003),
+(106, 'Dwapara', 3.54, 'Dance', 1002),
+(107, 'Sa Re Ga Ma', 4.20, 'Rhythmic', 1004),
+(108, 'Tauba', 4.05, 'Rhythmic', 1005),
+(109, 'Perfect', 4.23, 'Pop', 1002),
+(110, 'Good Luck', 3.55, 'Rhythmic', 1004);
+
+
+--1. Retrieve a unique genre of songs.
+select distinct Genre 
+from Songs
+
+--2. Find top 2 albums released before 2010.
+select top 2 * 
+from Albums
+where Release_year<2010
+
+--3. Insert Data into the Songs Table. (1245, �Zaroor�, 2.55, �Feel good�, 1005)
+insert into Songs values 
+(1245, 'Zaroor', 2.55, 'Feel good', 1005)
+
+--4. Change the Genre of the song �Zaroor� to �Happy�
+update Songs
+set Genre = 'happy'
+where Song_title = 'Zaroor'
+select * from Songs
+
+--5. Delete an Artist �Ed Sheeran�
+delete from Artists 
+where Artist_name='Ed Sheeran'
+
+--6. Add a New Column for Rating in Songs Table. [Ratings decimal(3,2)]
+alter table Songs 
+add Rating decimal(3,1)
+
+--7. Retrieve songs whose title starts with 'S'.
+select Song_title
+from Songs
+where Song_title like 'S%'
+
+--8. Retrieve all songs whose title contains 'Everybody'.
+select *
+from Songs
+where Song_title like '%Everybody%'
+
+--9. Display Artist Name in Uppercase.
+select upper("Artist_name")
+from Artists
+
+--10. Find the Square Root of the Duration of a Song �Good Luck�
+select square("Duration")
+from Songs
+where Song_title='Good Luck'
+
+--11. Find Current Date.
+select GETDATE()
+
+--12. Find the number of albums for each artist.
+select Artist_name , count(*)
+from Artists as a
+inner join Albums as al
+on a.Artist_id=al.Artist_id
+group by Artist_name
+
+--13. Retrieve the Album_id which has more than 5 songs in it.
+select a.Album_id, count(*)
+from Songs as s
+inner join Albums as a
+on s.Album_id=a.Album_id
+group by a.Album_id
+having COUNT(*)>5
+
+
+--14. Retrieve all songs from the album 'Album1'. (using Subquery)
+select * 
+from Songs
+where Album_id IN (select Album_id from Albums where Album_title='Album1')
+
+--15. Retrieve all albums name from the artist �Aparshakti Khurana� (using Subquery)
+select * 
+from Albums 
+where Artist_id IN (select Artist_id from Artists where Artist_name='Aparshakti Khurana')
+
+--16. Retrieve all the song titles with its album title.
+select a.Album_title, s.Song_title
+from Songs as s
+inner join Albums as a
+on s.Album_id=a.Album_id
+
+--17. Find all the songs which are released in 2020.
+select * 
+from Songs as s
+inner join Albums as a
+on s.Album_id=a.Album_id
+where a.Release_year=2020
+
+--18. Create a view called �Fav_Songs� from the songs table having songs with song_id 101-105.
+create view Fav_Songs 
+as
+select * 
+from Songs 
+where Song_id between 101 and 105
+
+select * from Fav_Songs
+--19. Update a song name to �Jannat� of song having song_id 101 in Fav_Songs view.
+update Fav_Songs
+set Song_title='Jannat'
+where Song_id=101
+
+--20. Find all artists who have released an album in 2020.
+select * 
+from Albums as a
+inner join Artists as ar
+on a.Artist_id=ar.Artist_id
+where a.Release_year=2020 
+
+--21. Retrieve all songs by Shreya Ghoshal and order them by duration. 
+select * 
+from Songs as s
+inner join Albums as a
+on a.Album_id=s.Album_id
+inner join Artists as ar
+on a.Artist_id=ar.Artist_id
+where  ar.Artist_name='Shreya Ghoshal'
+
+---------------------------------------------------------B category---------------------------------------
+
+--22. Retrieve all song titles by artists who have more than one album.
+select ar.Artist_name,s.Song_title
+from Songs as s
+inner join Albums as a
+on a.Album_id=s.Album_id
+inner join Artists as ar
+on a.Artist_id=ar.Artist_id
+group by ar.Artist_name,s.Song_title
+having count(DISTINCT a.Album_id)>1
+
+--23. Retrieve all albums along with the total number of songs.
+select a.Album_title,COUNT(*)
+from Songs as s
+inner join Albums as a
+on a.Album_id=s.Album_id
+group by a.Album_title
+
+--24. Retrieve all songs and release year and sort them by release year.
+select a.Album_title,a.Release_year
+from Albums as a
+order by a.Release_year
+
+--25. Retrieve the total number of songs for each genre, showing genres that have more than 2 songs.
+select count(*),genre
+from Songs 
+group by genre
+having COUNT(*)>2
+
+
+--26. List all artists who have albums that contain more than 3 songs.
+select Artist_name,Album_title
+from Artists as ar
+inner join Albums as a
+on ar.Artist_id=a.Artist_id
+inner join Songs as s
+on s.Album_id=a.Album_id
+group by Artist_name,Album_title
+having COUNT(s.Song_id)>2
+
+
+--------------------------------------------- c category----------------------------------------------
+
+--27. Retrieve albums that have been released in the same year as 'Album4'
+select Album_title
+from Albums as a
+where Release_year in (select Release_year from Albums where Album_title='Album4')
+
+--28. Find the longest song in each genre
+select Genre,max(len(Song_title))
+from Songs
+group by Genre
+--29. Retrieve the titles of songs released in albums that contain the word 'Album' in the title.
+select s.Song_titlefrom Songs as sinner join Albums as aon a.Album_id=s.Album_idwhere a.Album_title like '%Album%'--30. Retrieve the total duration of songs by each artist where total duration exceeds 15 minutes.select ar.Artist_name,sum(s.Duration)from Songs as sinner join Albums as aon a.Album_id=s.Album_idinner join Artists as aron ar.Artist_id=a.Artist_idgroup by ar.Artist_namehaving sum(s.Duration)>15
---
+++ lab2.sql
@@ -0,0 +1,375 @@
+﻿-- Create Department Table
+CREATE TABLE Department (
+ DepartmentID INT PRIMARY KEY,
+ DepartmentName VARCHAR(100) NOT NULL UNIQUE
+);
+
+--insert dep
+create or alter procedure pr_insert_Department
+ @did int, @dname varchar(50)
+as
+begin
+	insert into Department values(@did , @dname);
+end
+
+
+--update dep
+create or alter procedure pr_update_Department
+ @did int,@dname varchar(50)
+as
+begin
+	update Department
+	set DepartmentName=@dname
+	where DepartmentID = @did 
+end
+
+--delete dep
+create or alter procedure pr_delete_Department
+ @did int
+as
+begin
+	delete from Department
+	where DepartmentID = @did 
+end
+
+pr_insert_Department 1,'Admin'
+pr_insert_Department 2,'IT'
+pr_insert_Department 3,'HR'
+pr_insert_Department 4,'Acount'
+pr_update_Department 4,'Account'
+pr_delete_Department 3
+
+select * from Department 
+
+-- Create Designation Table
+
+CREATE TABLE Designation (
+ DesignationID INT PRIMARY KEY,
+ DesignationName VARCHAR(100) NOT NULL UNIQUE
+);
+
+
+--insert des
+create or alter procedure pr_insert_Designation
+ @dsid int, @dsname varchar(50)
+as
+begin
+	insert into Designation values(@dsid , @dsname);
+end
+--update des
+create or alter procedure pr_update_Designation
+ @dsid int,@dsname varchar(50)
+as
+begin
+	update Designation
+	set DesignationName=@dsname
+	where DesignationID = @dsid 
+end
+
+--delete des
+create or alter procedure pr_delete_Designation
+ @dsid int
+as
+begin
+	delete from Designation
+	where DesignationID = @dsid 
+end
+
+pr_update_Designation 11,'Robber'
+pr_insert_Designation 11,'Jobber'
+pr_insert_Designation 12,'Welder'
+pr_insert_Designation 13,'Clerk'
+pr_insert_Designation 14,'Manager'
+pr_insert_Designation 15,'CEO'
+
+select * from Designation
+
+-- Create Person Table
+CREATE TABLE Person (
+ PersonID INT PRIMARY KEY IDENTITY(101,1),
+ FirstName VARCHAR(100) NOT NULL,
+ LastName VARCHAR(100) NOT NULL,
+ Salary DECIMAL(8, 2) NOT NULL,
+ JoiningDate DATETIME NOT NULL,
+ DepartmentID INT NULL,
+ DesignationID INT NULL,
+ FOREIGN KEY (DepartmentID) REFERENCES Department(DepartmentID),
+ FOREIGN KEY (DesignationID) REFERENCES Designation(DesignationID)
+);
+
+--insert per
+create or alter procedure pr_insert_Person
+  @fn varchar(40),@ln varchar(40),@sal DECIMAL(8, 2),@jd datetime,@did int,@dsid int
+as
+begin
+	insert into Person values (@fn,@ln,@sal,@jd,@did,@dsid);
+end
+
+
+--update per
+create or alter procedure pr_update_Person
+ @pid int,@fname varchar(50),@lname varchar(50),@sal DECIMAL(8, 2),@jd datetime,@did int,@dsid int
+as
+begin
+	update Person
+	set FirstName=@fname,LastName= @lname,Salary = @sal, JoiningDate = @jd, DesignationID = @dsid , DepartmentID = @did
+	where PersonID = @pid 
+end
+
+--delete des
+create or alter procedure pr_delete_Person
+ @pid int
+as
+begin
+	delete from Person
+	where PersonID = @pid 
+end
+
+pr_insert_Person  'Rahul' ,'Anshu', 56000, '1990-01-01' ,1 ,12
+pr_insert_Person 'Hardik', 'Hinsu', 18000, '1990-09-25', 2,11
+pr_insert_Person 'Bhavin' ,'Kamani', 25000, '1991-05-14' ,NULL, 11
+pr_insert_Person 'Bhoomi', 'Patel', 39000, '2014-02-20', 1, 13
+pr_insert_Person 'Rohit', 'Rajgor', 17000, '1990-07-23', 2, 15
+pr_insert_Person 'Priya', 'Mehta', 25000, '1990-10-18', 2, NULL
+pr_insert_Person 'Neha', 'Trivedi', 18000, '2014-02-20', 3, 15
+pr_update_Person 107,'Neha', 'Trivedi', 19000, '2014-02-20', 3, 15
+pr_insert_Person 'Neha', 'Trivedi', 19000, '2014-02-20', 3, 15
+pr_delete_Person 108
+
+select * from Person
+
+--2. Department, Designation & Person Table�s SELECTBYPRIMARYKEY
+create or alter procedure pr_SELECTBYPRIMARYKEY_person
+ @pid int
+as
+begin
+
+	select *
+	from Person as p
+	where p.PersonID=@pid
+end
+
+pr_SELECTBYPRIMARYKEY_person 101
+
+create or alter procedure pr_SELECTBYPRIMARYKEY_Dept
+ @did int
+as
+begin
+
+	select *
+	from Department 
+	where DepartmentID=@did
+end
+
+pr_SELECTBYPRIMARYKEY_Dept 1
+
+create or alter procedure pr_SELECTBYPRIMARYKEY_Design
+ @dsid int
+as
+begin
+
+	select *
+	from Designation
+	where DesignationID=@dsid
+end
+
+pr_SELECTBYPRIMARYKEY_Design 11
+
+--3. Department, Designation & Person Table�s (If foreign key is available then do write join and take
+--columns on select list)
+create or alter procedure pr_full
+as
+begin
+
+	select *
+	from Person as p
+	inner join Department as d
+	on d.DepartmentID = p.DepartmentID
+	inner join Designation as ds
+	on ds.DesignationID = p.DesignationID
+	
+end
+
+pr_full
+
+--4. Create a Procedure that shows details of the first 3 persons.
+
+create or alter procedure pr_detail
+as
+begin
+
+	select top 3 *
+	from Person 
+	
+end
+
+pr_detail
+
+
+
+-------------------------------------------------b Categroy----------------------------
+
+--5. Create a Procedure that takes the department name as input and returns a table with all workers
+--working in that department.
+
+create or alter procedure pr_get_person
+@dname varchar(40)
+as
+begin 
+	select * 
+	from Person as p
+	inner join Department as d
+	on p.DepartmentID = d.DepartmentID
+	where d.DepartmentName = @dname
+end
+
+pr_get_person 'IT'
+
+--6. Create Procedure that takes department name & designation name as input and returns a table with
+--worker’s first name, salary, joining date & department name.
+create or alter procedure pr_get_person_ds
+@dname varchar(40),@dsname varchar(50)
+as
+begin 
+	select p.salary,p.FirstName,p.JoiningDate,d.DepartmentName 
+	from Person as p
+	inner join Department as d
+	on p.DepartmentID = d.DepartmentID
+	inner join Designation as ds
+	on p.DesignationID = ds.DesignationID
+	where d.DepartmentName = @dname and ds.DesignationName = @dsname
+end
+pr_get_person_ds 'IT','Jobber'
+
+--7. Create a Procedure that takes the first name as an input parameter and display all the details of the
+--worker with their department & designation name.
+
+create or alter procedure pr_get_person_fn
+@fname varchar(40)
+as
+begin 
+	select p.salary,p.FirstName,p.JoiningDate,d.DepartmentName,ds.DesignationName 
+	from Person as p
+	inner join Department as d
+	on p.DepartmentID = d.DepartmentID
+	inner join Designation as ds
+	on p.DesignationID = ds.DesignationID
+	where p.FirstName = @fname
+end
+
+pr_get_person_fn 'Rahul'
+
+--8. Create Procedure which displays department wise maximum, minimum & total salaries.
+create or alter procedure pr_salary
+as
+begin 
+	select d.DepartmentName,max(p.salary),min (p.Salary),sum(p.salary) 
+	from Person as p
+	inner join Department as d
+	on p.DepartmentID = d.DepartmentID
+	group by d.DepartmentName
+end
+
+pr_salary 
+
+--9. Create Procedure which displays designation wise average & total salaries.
+
+
+create or alter procedure pr_avg
+as
+begin 
+	select sum(p.salary),avg(p.salary),ds.DesignationName 
+	from Person as p
+	inner join Designation as ds
+	on p.DesignationID = ds.DesignationID
+	group by ds.DesignationName
+end
+
+pr_avg
+
+-----------------------------------------------------c category------------------------------------
+--10. Create Procedure that Accepts Department Name and Returns Person Count.
+create or alter procedure pr_count
+@dname varchar(40)
+as
+begin 
+	select d.DepartmentName,count(*)
+	from Person as p
+	inner join Department as d
+	on p.DepartmentID = d.DepartmentID
+	group by d.DepartmentName
+	having d.DepartmentName = @dname
+end
+pr_count 'IT'
+--11. Create a procedure that takes a salary value as input and returns all workers with a salary greater than
+--input salary value along with their department and designation details.
+create or alter procedure pr_take_salary_return_detail
+@sal int
+as
+begin 
+	select p.salary,d.DepartmentName,ds.DesignationName 
+	from Person as p
+	inner join Department as d
+	on p.DepartmentID = d.DepartmentID
+	inner join Designation as ds
+	on p.DesignationID = ds.DesignationID
+	where p.salary>@sal
+end
+pr_take_salary_return_detail '2'
+
+--12. Create a procedure to find the department(s) with the highest total salary among all departments.
+create or alter procedure pr_return_highest_salary
+as
+begin 
+	select max(p.salary)
+	from Person as p
+	inner join Department as d
+	on p.DepartmentID = d.DepartmentID
+end
+pr_return_highest_salary 
+
+--13. Create a procedure that takes a designation name as input and returns a list of all workers under that
+--designation who joined within the last 10 years, along with their department.
+create or alter procedure pr_take_designationName
+@dsname varchar(40)
+as
+begin 
+	select d.DepartmentName,p.FirstName
+	from Person as p
+	inner join Designation as ds
+	on p.DesignationID = ds.DesignationID
+	inner join Department as d
+	on p.DepartmentID = d.DepartmentID
+	where ds.DesignationName = @dsname and datediff(year,p.JoiningDate,getdate())<=10
+end
+
+pr_take_designationName 'Clerk'
+--14. Create a procedure to list the number of workers in each department who do not have a designation
+--assigned.
+create or alter procedure pr_get_null
+as
+begin 
+	select p.FirstName,d.DepartmentName
+	from Person as p
+	inner join Department as d
+	on p.DepartmentID = d.DepartmentID
+	where p.DesignationID is null
+end
+
+pr_get_null
+--15. Create a procedure to retrieve the details of workers in departments where the average salary is above
+--12000.
+create or alter procedure pr_retrieve
+as
+begin 
+	select p.FirstName,d.DepartmentName
+	from Person as p
+	inner join Department as d
+	on p.DepartmentID = d.DepartmentID
+	inner join Designation as ds
+	on p.DesignationID = ds.DesignationID
+	group by d.DepartmentName,p.FirstName
+	having avg(p.salary)>12000
+end
+
+pr_retrieve
